a0919d7188215fea53c9fda6277df3558b402257
Alan Scherger <alan.scherger@gmail.com>
2024-04-11 22:12:48 -0500

    [feat] config template and cmd dir fix

---
+++ cmd/pkl-go-bananas/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"context"
+	_ "embed"
+	"fmt"
+	"github.com/flyinprogrammer/pkl-go-bananas/gen/appconfig"
+	"github.com/flyinprogrammer/pkl-go-bananas/internal"
+	"log"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+func main() {
+	configFilePath := setupLocalConfigFile()
+	cfg, err := appconfig.LoadFromPath(context.TODO(), configFilePath)
+	if err != nil {
+		log.Fatalf("Error loading config file: %v", err)
+	}
+	printNetworkData(cfg)
+	sleepForContainerTesting()
+}
+
+func setupLocalConfigFile() (configFilePath string) {
+	cfgDir, err := os.UserConfigDir()
+	if err != nil {
+		log.Fatalf("Error getting user config directory: %v", err)
+	}
+
+	cliCfgDirPath := filepath.Join(cfgDir, "pkl-go-bananas")
+	if _, err := os.Stat(cliCfgDirPath); os.IsNotExist(err) {
+		if err := os.Mkdir(cliCfgDirPath, 0700); err != nil {
+			log.Fatalf("Error creating cli config dir: %v", err)
+		}
+	}
+
+	configFilePath = filepath.Join(cliCfgDirPath, "config.pkl")
+	if _, err := os.Stat(configFilePath); os.IsNotExist(err) {
+		data := internal.DefaultTemplate
+		err = os.WriteFile(configFilePath, []byte(data), 0600)
+		if err != nil {
+			log.Fatalf("Error creating default config file: %v", err)
+		}
+	}
+	return
+}
+
+func printNetworkData(cfg *appconfig.AppConfig) {
+	fmt.Println("Let's print out your network data:")
+	for _, computer := range cfg.Computers {
+		fmt.Printf("Computer: %s\n", computer.Name)
+		for _, ip := range computer.IpAddress {
+			fmt.Printf("\tIP: %s\n", ip)
+		}
+	}
+}
+
+func sleepForContainerTesting() {
+	value, exists := os.LookupEnv("PKL_GO_BANANAS_SLEEP")
+	if exists {
+		sleepTime, err := time.ParseDuration(value)
+		if err != nil {
+			log.Fatalf("Error parsing sleep time (https://pkg.go.dev/maze.io/x/duration#ParseDuration): %v", err)
+		}
+		log.Printf("Sleeping for %s.", sleepTime)
+		time.Sleep(sleepTime)
+	}
+}
---
+++ internal/defaultTemplate.go
@@ -0,0 +1,6 @@
+package internal
+
+import _ "embed"
+
+//go:embed config.default.pkl
+var DefaultTemplate string
